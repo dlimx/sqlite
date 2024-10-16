a6c66ba4c776cd4d44a62d2debfe25da15f109ce
David Li <me@davidli.io>
2024-10-15 21:18:36 -0400

    add cell based table count
    test
    
    [any message]

--- src/Server.cpp
+++ src/Server.cpp
@@ -27,13 +27,21 @@ int main(int argc, char* argv[]) {
 
         // Uncomment this to pass the first stage
         database_file.seekg(16);  // Skip the first 16 bytes of the header
-        // 
-        char buffer[2];
-        database_file.read(buffer, 2);
         
-        unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
+        char page_buffer[2];
+        database_file.read(page_buffer, 2);
+        
+        unsigned short page_size = (static_cast<unsigned char>(page_buffer[1]) | (static_cast<unsigned char>(page_buffer[0]) << 8));
+
+        database_file.seekg(103);  // Skip the first 28 bytes of the header
+        
+        char table_buffer[2];
+        database_file.read(table_buffer, 2);
+
+        unsigned int table_count = (static_cast<unsigned char>(table_buffer[1]) | (static_cast<unsigned char>(table_buffer[0]) << 8));
         
         std::cout << "database page size: " << page_size << std::endl;
+        std::cout << "number of tables: " << table_count << std::endl;
     }
 
     return 0;
